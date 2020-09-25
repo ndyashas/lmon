47cc04041a9c05da3297b07167f512dbd24c3198
Yashas ND <ndyashas@gmail.com>
2020-09-25 13:24:03 +0530

    Fixed bug, where the while loop breaks after first ssh.
    Reference: https://stackoverflow.com/questions/9393038/ssh-breaks-out-of-while-loop-in-bash

--- data-collection/main.sh
+++ data-collection/main.sh
@@ -8,7 +8,7 @@ do
     HOSTPATH="${DIR}"/log/"$(date '+%d-%m-%Y')/${HOST}"
     mkdir -p "${HOSTPATH}";
     echo "Collecting from ${HOST}";
-    bash "${DIR}"/data-collection/utils/cpu-usage.sh "${HOST}" > "${HOSTPATH}"/cpu-usage.csv;
-    bash "${DIR}"/data-collection/utils/mem-usage.sh "${HOST}" > "${HOSTPATH}"/mem-usage.csv;
-    bash "${DIR}"/data-collection/utils/last-login-info.sh "${HOST}" > "${HOSTPATH}"/last_login_info.txt
+    bash "${DIR}"/data-collection/utils/cpu-usage.sh "${HOST}" > "${HOSTPATH}"/cpu-usage.csv < /dev/null;
+    bash "${DIR}"/data-collection/utils/mem-usage.sh "${HOST}" > "${HOSTPATH}"/mem-usage.csv < /dev/null;
+    bash "${DIR}"/data-collection/utils/last-login-info.sh "${HOST}" > "${HOSTPATH}"/last_login_info.txt < /dev/null;
 done < "${MACHINEFILE}"
