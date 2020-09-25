db3aa95f84d3d4f570285286f28cfe9920965731
Sneha Rao GR <sneharaog@gmail.com>
2020-09-25 13:26:45 +0530

    [BUG FIX] In the main.sh, modified to collect yesterday's data.

--- data-collection/main.sh
+++ data-collection/main.sh
@@ -5,7 +5,7 @@ MACHINEFILE="${DIR}"/config/machinefile
 
 while read HOST
 do
-    HOSTPATH="${DIR}"/log/"$(date '+%d-%m-%Y')/${HOST}"
+    HOSTPATH="${DIR}"/log/"$(date --date="yesterday" '+%d-%m-%Y')/${HOST}"
     mkdir -p "${HOSTPATH}";
     echo "Collecting from ${HOST}";
     bash "${DIR}"/data-collection/utils/cpu-usage.sh "${HOST}" > "${HOSTPATH}"/cpu-usage.csv;
