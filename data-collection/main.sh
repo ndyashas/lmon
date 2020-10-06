4650ba93a10ea7a4b069cc6c45879d52940a2b9a
Yashas ND <ndyashas@gmail.com>
2020-10-06 20:24:52 +0530

    Collect hostname data

--- data-collection/main.sh
+++ data-collection/main.sh
@@ -11,4 +11,5 @@ do
     bash "${DIR}"/data-collection/utils/cpu-usage.sh "${HOST}" > "${HOSTPATH}"/cpu-usage.csv < /dev/null;
     bash "${DIR}"/data-collection/utils/mem-usage.sh "${HOST}" > "${HOSTPATH}"/mem-usage.csv < /dev/null;
     bash "${DIR}"/data-collection/utils/last-login-info.sh "${HOST}" > "${HOSTPATH}"/last_login_info.txt < /dev/null;
+    bash "${DIR}"/data-collection/utils/hostname.sh "${HOST}" > "${HOSTPATH}"/hostname.txt < /dev/null;
 done < "${MACHINEFILE}"
