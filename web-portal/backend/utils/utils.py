be9a7bea7bc99a9930e8079c22195b1b1d05d307
Yashas ND <ndyashas@gmail.com>
2020-10-07 12:09:00 +0530

    Removed the legacy endpoint that is no longer useful.

--- web-portal/backend/utils/utils.py
+++ web-portal/backend/utils/utils.py
@@ -6,15 +6,6 @@ class Util:
     def __init__(self):
         self._lmon_path = os.path.join(os.environ.get('HOME'), ".lmon")
         self._log_dir = os.path.join(self._lmon_path, "log")
-        
-    def get_num_of_machines(self, date):
-        """Return an integer showing the number of machines from
-        which data was collected.
-        
-        Return 0 if no data was collected on the given day.
-        """
-        machines_list = glob.glob(os.path.join(self._log_dir, date, "*"))
-        return len(machines_list)
 
     def get_machines(self, date):
         """Return a dictionary of machines from which data was 
