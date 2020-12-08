ac28f133946ff2742c6c0472cb645307eb23c777
Yashas ND <ndyashas@gmail.com>
2020-12-08 17:24:26 +0530

    Handle the case of having the machine ids specified as user@ip

--- data-collection/utils/sys-up-tests.sh
+++ data-collection/utils/sys-up-tests.sh
@@ -3,10 +3,10 @@
 
 ping_test()
 {
-    ping -c 10 "${1}" &> /dev/null
+    ping -c 10 "${1##*@}" &> /dev/null
 }
 
 check_ssh()
 {
-    nc -4 -d -z -w 15 "${1}" 22 &> /dev/null
+    nc -4 -d -z -w 15 "${1##*@}" 22 &> /dev/null
 }
